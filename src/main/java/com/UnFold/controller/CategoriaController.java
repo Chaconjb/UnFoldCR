1ec22f7e1921c39bc6b57542f906021788f31221
AlexandraC09 <camposalee.09@gmail.com>
2025-07-10 22:52:28 -0600

    Producto

--- src/main/java/com/UnFold/controller/CategoriaController.java
+++ src/main/java/com/UnFold/controller/CategoriaController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes; // Para mensajes de éxito/error en redirecciones
+import org.springframework.web.servlet.mvc.support.RedirectAttributes; 
 
 import java.util.List;
 import java.util.stream.Collectors;
